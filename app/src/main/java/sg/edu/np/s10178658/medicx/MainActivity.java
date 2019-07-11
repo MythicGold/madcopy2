0ea77a285e32bfb3584ca95790da28eef1b084e0
Tze Kang Gerald Leow <s10178658@connect.np.edu.sg>
2019-07-12 00:23:46 +0800

    Login Page ver 1

--- app/src/main/java/sg/edu/np/s10178658/medicx/MainActivity.java
+++ app/src/main/java/sg/edu/np/s10178658/medicx/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -17,6 +18,9 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "AndroidClarified";
@@ -66,6 +70,7 @@ public class MainActivity extends AppCompatActivity {
     private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
         try {
             GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+
             // Signed in successfully, show authenticated UI.
             startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         } catch (ApiException e) {
@@ -86,5 +91,31 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(MainActivity.this, ProfileActivity.class));
         }
         super.onStart();
+    }
+
+    public void onClick(View v)
+    {
+        EditText et1 = findViewById(R.id.editText);
+        EditText et2 = findViewById(R.id.editText2);
+        String userName = et1.getText().toString();
+        String password = et2.getText().toString();
+
+        Pattern pattern = Pattern.compile("^[0-9a-zA-Z]{6,12}$"); //means must be between 6-12 characters and alpha numeric
+        Matcher matcher = pattern.matcher(userName);
+
+        Pattern pattern2 = Pattern.compile("^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*+=]).*$"); //means at least 1 symbol, 1 uppercase, 1 numeric
+        Matcher matcher2 = pattern2.matcher(password);
+
+        //calling matches method, return true or false, searching for the pattern, and display output
+        if(matcher.matches() == true && matcher2.matches() == true)
+        {
+            Toast tt = Toast.makeText(MainActivity.this, "Valid Login Credentials", Toast.LENGTH_LONG); // so when timer runs out, show toast notification that timer has expired LENGTH_LONG will appear for 3 seconds
+            tt.show(); // like countdown timer, must show
+        }
+        else
+        {
+            Toast tt = Toast.makeText(MainActivity.this, "Invalid Login Credentials", Toast.LENGTH_LONG); // so when timer runs out, show toast notification that timer has expired LENGTH_LONG will appear for 3 seconds
+            tt.show(); // like countdown timer, must show
+        }
     }
 }
