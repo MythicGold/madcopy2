28fd961fc345917c135357633607d2c651cb7199
Rui Xin <Rui Xin@LAPTOP-J246S19M>
2019-07-17 02:31:57 +0800

    Queue Ticket num generator

---
+++ app/src/main/java/sg/edu/np/s10178658/medicx/QueueTicket.java
@@ -0,0 +1,17 @@
+package sg.edu.np.s10178658.medicx;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.security.SecureRandom;
+
+public class QueueTicket extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_queue_ticket);
+
+
+    }
+}
---
+++ app/src/main/java/sg/edu/np/s10178658/medicx/RandomTicketNumber.java
@@ -0,0 +1,22 @@
+package sg.edu.np.s10178658.medicx;
+
+import java.security.SecureRandom;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Random;
+
+public class RandomTicketNumber {
+
+    private static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    public String generateString(int length) {
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            builder.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+        }
+
+        return builder.toString();
+    }
+}
